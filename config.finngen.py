dafe486384bdb61ad465ca352b79f3f2851a9b38
juhis <jkarjala@broadinstitute.org>
2024-04-15 06:59:38 +0000

    support different configs

--- config.finngen.py
+++ config.finngen.py
@@ -3,8 +3,9 @@ import logging
 log_level = logging.INFO
 
 authentication = False
+authentication_file = "/mnt/disks/data/finngen_auth.json"
 
-metadata_db = "/mnt/disks/data/meta_public_version_20240219.db"
+metadata_db = "/mnt/disks/data/meta_finngen_version_20240219.db"
 
 rsid_db = {
     "file": "/mnt/disks/data/gnomad/gnomad.genomes.exomes.v4.0.rsid.db",
@@ -18,23 +19,23 @@ gnomad = {
 }
 
 assoc = {
-    "file": "/mnt/disks/data/assoc_resources_public_version_20240219.tsv.gz",
+    "file": "/mnt/disks/data/assoc_resources_finngen_version_20240219.tsv.gz",
     # not all resources in the data file need to be listed here
     # if a resource is not listed here, data for it will not be shown in the UI
     "resources": [
         {
             "resource": "FinnGen_UKBB_meta",
-            "version": "R10",
+            "version": "R12",
             "data_types": ["GWAS"],
-            "n_traits": "694",
-            "url": "https://finngen.gitbook.io/documentation",
+            "n_traits": "873",
+            "url": "https://www.finngen.fi/en/for_researchers",
             "pheno_urls": [
                 {
-                    "url": "https://public-metaresults-fg-ukbb.finngen.fi/pheno/[PHENOCODE]",
+                    "url": "https://metaresults-ukbb.finngen.fi/pheno/[PHENOCODE]",
                     "label": "FinnGen/UKBB meta Pheweb",
                 },
                 {
-                    "url": "https://r10.risteys.finregistry.fi/endpoints/[PHENOCODE]",
+                    "url": "https://r12.risteys.finregistry.fi/endpoints/[PHENOCODE]",
                     "label": "FinnGen Risteys",
                 },
             ],
@@ -42,19 +43,62 @@ assoc = {
         },
         {
             "resource": "FinnGen",
-            "version": "R10",
+            "version": "R12",
             "data_types": ["GWAS"],
-            "n_traits": "2,408",
-            "url": "https://finngen.gitbook.io/documentation",
+            "n_traits": "2,469",
+            "url": "https://www.finngen.fi/en/for_researchers",
             "pheno_urls": [
                 {
-                    "url": "https://r10.finngen.fi/pheno/[PHENOCODE]",
+                    "url": "https://r12.finngen.fi/pheno/[PHENOCODE]",
                     "label": "FinnGen Pheweb",
                 },
                 {
-                    "url": "https://r10.risteys.finregistry.fi/endpoints/[PHENOCODE]",
+                    "url": "https://r12.risteys.finregistry.fi/endpoints/[PHENOCODE]",
                     "label": "FinnGen Risteys",
                 },
+            ],
+            "p_thres": 5e-3,
+        },
+        {
+            "resource": "FinnGen_pQTL",
+            "version": "SomaScan 2023-03-02, Olink 2023-10-11",
+            "data_types": ["pQTL"],
+            "n_traits": "SomaScan 7,596, Olink 2,925",
+            "url": "https://www.finngen.fi/en/for_researchers",
+            "pheno_urls": [
+                {
+                    "url": "https://r12.finngen.fi/gene/[GENE]",
+                    "label": "FinnGen Pheweb",
+                }
+            ],
+            "p_thres": 5e-3,
+        },
+        {
+            "resource": "FinnGen_eQTL",
+            "version": "2023-10-05",
+            "data_types": ["eQTL"],
+            "n_traits": "26,624 genes, 35 predicted cell types",
+            "url": "https://www.finngen.fi/en/for_researchers",
+            "pheno_urls": [
+                {
+                    "url": "https://r12.finngen.fi/gene/[GENE]",
+                    "label": "FinnGen Pheweb",
+                }
+            ],
+            "p_thres": 5e-3,
+        },
+        {
+            "resource": "UKBB_pQTL",
+            "version": "2024-01-30",
+            "data_types": ["pQTL"],
+            "n_traits": "Olink 2,655",
+            # "url": "https://www.biorxiv.org/content/10.1101/2022.06.17.496443v1.full",
+            "url": None,
+            "pheno_urls": [
+                {
+                    "url": "https://www.finngen.fi/en/for_researchers",
+                    "label": "FinnGen analysis",
+                }
             ],
             "p_thres": 5e-3,
         },
@@ -101,21 +145,6 @@ assoc = {
             "p_thres": 5e-3,
         },
         {
-            "resource": "UKBB_pQTL",
-            "version": "2024-01-30",
-            "data_types": ["pQTL"],
-            "n_traits": "Olink 2,655",
-            # "url": "https://www.biorxiv.org/content/10.1101/2022.06.17.496443v1.full",
-            "url": None,
-            "pheno_urls": [
-                {
-                    "url": "https://www.finngen.fi/en/for_researchers",
-                    "label": "FinnGen analysis",
-                }
-            ],
-            "p_thres": 5e-3,
-        },
-        {
             "resource": "GTEx_v8_edQTL",
             "version": "2022",
             "data_types": ["edQTL"],
@@ -133,7 +162,7 @@ assoc = {
 }
 
 finemapped = {
-    "file": "/mnt/disks/data/finemapped_resources_public_version_20240219.tsv.gz",
+    "file": "/mnt/disks/data/finemapped_resources_finngen_version_20240219.tsv.gz",
     "resources": [
         {
             "resource": "eQTL_Catalogue_R6",
@@ -144,9 +173,23 @@ finemapped = {
         },
         {
             "resource": "FinnGen",
-            "version": "R10",
+            "version": "R12",
             "data_types": ["GWAS"],
-            "n_traits": "2,408",
+            "n_traits": "2,469",
+            "url": "https://www.finngen.fi/en/for_researchers",
+        },
+        {
+            "resource": "FinnGen_pQTL",
+            "version": "SomaScan 2023-03-02, Olink 2023-10-11",
+            "data_types": ["pQTL"],
+            "n_traits": "SomaScan 7,596, Olink 2,925",
+            "url": "https://www.finngen.fi/en/for_researchers",
+        },
+        {
+            "resource": "FinnGen_eQTL",
+            "version": "2023-10-05",
+            "data_types": ["eQTL"],
+            "n_traits": "26,624 genes, 35 predicted cell types",
             "url": "https://www.finngen.fi/en/for_researchers",
         },
         {
--- src/features/page/ChangeLog.tsx
+++ src/features/page/ChangeLog.tsx
@@ -1,59 +1,38 @@
-const ChangeLog = () => {
+import config from "../../config.json";
+let changeLog: { content: Array<ChangeLogItem> };
+if (config.target === "public") {
+  changeLog = (await import("../../../changelog.json")).default;
+} else {
+  changeLog = (await import("../../../changelog_finngen.json")).default;
+}
+import { ChangeLogItem } from "../../types/types";
+
+const renderContent = (content: Array<ChangeLogItem>) => {
   return (
     <>
-      <span>Feb 27th 2024:</span>
-      <ul>
-        <li>
-          Updated UKBB pQTL association and fine-mapping results from 1,459 to 2,655 proteins
-          (analysis by Zhili Zheng / FinnGen)
-        </li>
-        <li>
-          Unassigned population is now called "remaining" and not "oth" (in gnomAD, "oth" was used
-          until gnomAD 3 and "remaining" is used in gnomAD 4)
-        </li>
-      </ul>
-      <span>Jan 21st 2024:</span>
-      <ul>
-        <li>Added FinnGen R12 + UKBB meta-analysis association results</li>
-        <li>
-          Show gnomAD exomes allele frequency only when exomes allele number is higher than genomes
-          allele number
-        </li>
-        <li>Give a note when a variant is in gnomAD but has allele count zero</li>
-        <li>Updated rsids to gnomAD 4.0</li>
-        <li>Fixed population AF summary table</li>
-      </ul>
-      <span>Dec 19th 2023:</span>
-      <ul>
-        <li>
-          Updated allele frequencies to gnomAD 4.0 - exome data is used instead of genome data when
-          available
-        </li>
-        <li>Updated FinnGen EA5 priority variant list</li>
-        <li>Fixed extra "gene symbol" text sometimes appearing in the gene column</li>
-      </ul>
-      <span>Nov 6th 2023:</span>
-      <ul>
-        <li>Added FinnGen snRNA-seq eQTL results from the first 3 batches</li>
-        <li>Updated FinnGen Olink pQTL results from 2 batches to 3 batches</li>
-        <li>Updated FinnGen GWAS results from R11 to R12</li>
-        <li>Added last updated text to site header</li>
-      </ul>
-      <span>Sep 26th 2023:</span>
-      <ul>
-        <li>Release for FinnGen partners in the F2F meeting</li>
-        <li>Added UKBB pQTL results</li>
-        <li>Updates based on beta reviewer comments: </li>
-        <li>Fixed and enriched exported data tables</li>
-        <li>Added data type comparison table</li>
-        <li>Added gene description tooltip and other small improvements</li>
-      </ul>
-      <span>Aug 19th 2023:</span>
-      <ul>
-        <li>Closed beta review</li>
-      </ul>
+      {content.map((item, index) => {
+        switch (item.type) {
+          case "span":
+            return (
+              <span key={index}>
+                {item.text}
+                {item.children && renderContent(item.children)}
+              </span>
+            );
+          case "ul":
+            return <ul key={index}>{item.children && renderContent(item.children)}</ul>;
+          case "li":
+            return <li key={index}>{item.text}</li>;
+          default:
+            return null;
+        }
+      })}
     </>
   );
+};
+
+const ChangeLog = () => {
+  return renderContent(changeLog.content);
 };
 
 export default ChangeLog;
