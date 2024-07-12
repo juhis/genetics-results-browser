121bade5b60a27656e4bb4f179e45bdd49698a1e
juhis <jkarjala@broadinstitute.org>
2024-07-12 11:24:46 +0000

    fetch gene info on tooltip hover

--- src/features/tooltips/GeneToolTip.tsx
+++ src/features/tooltips/GeneToolTip.tsx
@@ -1,60 +1,69 @@
-import { Link, Typography } from "@mui/material";
+import { Link } from "@mui/material";
 import { useEffect, useState } from "react";
 import { GeneInfo } from "../../types/types";
 import { HtmlTooltip } from "./HtmlTooltip";
 
 const GeneTooltip = (props: { geneName: string; content: JSX.Element }) => {
-  const [geneInfo, setGeneInfo] = useState(undefined as GeneInfo | undefined);
+  const [geneInfo, setGeneInfo] = useState<GeneInfo | undefined>(undefined);
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
-    fetch(`https://mygene.info/v3/query?q=symbol:${props.geneName}`)
-      .then((result) => {
-        return result.json();
-      })
-      .then((data) => {
-        const geneId = data.hits[0]?._id;
-        if (!geneId) {
-          throw new Error(`${props.geneName}`);
-        }
-        return fetch(`https://mygene.info/v3/gene/${geneId}`);
-      })
-      .then((result) => {
-        return result.json();
-      })
-      .then((data) => {
-        setGeneInfo(data);
-      })
-      .catch((error) => {
-        setGeneInfo({
-          _id: "NA",
-          symbol: "NA",
-          name: "NA",
-          summary: error.message,
-        });
-      });
-  }, []);
+    if (isHovered) {
+      const fetchData = async () => {
+        fetch(`https://mygene.info/v3/query?q=symbol:${props.geneName}`)
+          .then((result) => {
+            return result.json();
+          })
+          .then((data) => {
+            const geneId = data.hits[0]?._id;
+            if (!geneId) {
+              throw new Error(`${props.geneName}`);
+            }
+            return fetch(`https://mygene.info/v3/gene/${geneId}`);
+          })
+          .then((result) => {
+            return result.json();
+          })
+          .then((data) => {
+            setGeneInfo(data);
+          })
+          .catch((error) => {
+            setGeneInfo({
+              _id: "NA",
+              symbol: "NA",
+              name: "NA",
+              summary: error.message,
+            });
+          });
+      };
+      fetchData();
+    }
+  }, [isHovered, props.geneName]);
 
-  return geneInfo === undefined ? (
-    <HtmlTooltip title="loading...">
-      <div>{props.geneName}</div>
-    </HtmlTooltip>
-  ) : geneInfo._id == "NA" ? (
-    <div>{geneInfo.summary}</div>
-  ) : (
-    <HtmlTooltip
-      title={
-        <div>
-          <div style={{ fontWeight: "bold" }}>{geneInfo?.symbol}</div>
-          <div>{geneInfo?.name}</div>
-          <br />
-          <div>{geneInfo?.summary}</div>
-          <div>
-            <Link href={`https://www.ncbi.nlm.nih.gov/gene/${geneInfo?._id}`}>NCBI</Link>
-          </div>
-        </div>
-      }>
-      {props.content}
-    </HtmlTooltip>
+  return (
+    <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <HtmlTooltip
+        title={
+          geneInfo === undefined ? (
+            "loading..."
+          ) : geneInfo._id == "NA" ? (
+            geneInfo.summary
+          ) : (
+            <div>
+              <div style={{ fontWeight: "bold" }}>{geneInfo?.symbol}</div>
+              <div>{geneInfo?.name}</div>
+              <br />
+              <div>{geneInfo?.summary}</div>
+              <div>
+                <Link href={`https://www.ncbi.nlm.nih.gov/gene/${geneInfo?._id}`}>NCBI</Link>
+              </div>
+            </div>
+          )
+        }>
+        <div>{geneInfo ? props.content : props.geneName}</div>
+      </HtmlTooltip>
+    </div>
   );
 };
+
 export default GeneTooltip;
